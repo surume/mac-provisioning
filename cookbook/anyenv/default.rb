abeb9757238597a12b7cf344f16a0da015a9857b
Surume <mail@sururne.com>
2015-08-07 02:07:20 +0900

    Add anyenv recipe

---
+++ cookbook/anyenv/default.rb
@@ -0,0 +1,21 @@
+ANYENV_PATH = "#{ENV['HOME']}/.anyenv"
+
+git ANYENV_PATH do
+  repository "https://github.com/riywo/anyenv"
+end
+
+git "#{ANYENV_PATH}/plugins/anyenv-update" do
+  repository "https://github.com/znz/anyenv-update.git"
+end
+
+directory "#{ANYENV_PATH}/envs"
+
+execute "install rbenv" do
+  command "#{ANYENV_PATH}/bin/anyenv install -f rbenv"
+  not_if "type rbenv"
+end
+
+execute "install exenv" do
+  command "#{ANYENV_PATH}/bin/anyenv install -f exenv"
+  not_if "type exenv"
+end

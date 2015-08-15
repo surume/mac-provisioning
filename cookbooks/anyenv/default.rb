94cf97263cd3c99c4b2cdf146847ebaa0d66b39a
surume <mail@sururne.com>
2015-08-16 03:59:26 +0900

    Refactor

--- cookbooks/anyenv/default.rb
+++ cookbooks/anyenv/default.rb
@@ -1,12 +1,3 @@
 node[:anyenv][:anyenv_root] += "/.anyenv"
 
 include_recipe "anyenv::system"
-
-node[:anyenv][:install_versions].each do |env_name, versions|
-  versions.each do |ver|
-    execute "#{env_name} install #{ver}" do
-      command "#{anyenv_init} #{env_name} install #{ver}"
-      not_if "#{anyenv_init} #{env_name} versions | grep #{ver}"
-    end
-  end
-end

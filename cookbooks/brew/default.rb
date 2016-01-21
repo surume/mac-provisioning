74f0b3c8d59ceb8c687c5294750cb658ed8fcccc
yourname <you@example.com>
2016-01-22 01:45:39 +0900

    Fix

--- cookbooks/brew/default.rb
+++ cookbooks/brew/default.rb
@@ -6,14 +6,14 @@ end
 include_recipe 'homebrew::package'
 include_recipe 'homebrew::cask'
 
-execute 'copy google-chrome to /Applications' do
-  command 'cp -rf /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app /Applications/'
-  only_if 'ls /opt/homebrew-cask/Caskroom | grep google-chrome'
-  not_if 'ls /Applications | grep Google\ Chrome.app'
-end
+# execute 'copy google-chrome to /Applications' do
+#   command 'cp -rf /opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app /Applications/'
+#   only_if 'ls /opt/homebrew-cask/Caskroom | grep google-chrome'
+#   not_if 'ls /Applications | grep Google\ Chrome.app'
+# end
 
-execute 'Setting ricty' do
-  command 'cp -f /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/'
-  command 'fc-cache -vf'
-  not_if 'ls ~/Library/Fonts/ | grep Ricty'
-end
+# execute 'Setting ricty' do
+#   command 'cp -f /usr/local/Cellar/ricty/*/share/fonts/Ricty*.ttf ~/Library/Fonts/'
+#   command 'fc-cache -vf'
+#   not_if 'ls ~/Library/Fonts/ | grep Ricty'
+# end
